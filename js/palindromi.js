812f3b024a15175f9da03eb21c54b7b928c1adfd
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-09 10:34:08 +0100

    Committed

--- js/palindromi.js
+++ js/palindromi.js
@@ -3,13 +3,14 @@ let parolaArray = []
 
 for (let i = 0; i < parolaUtente.length; i++) {
     parolaArray.push(parolaUtente[i])
-    console.log(parolaArray)
 }
 
-const parolaArrayReversed = parolaArray.reverse()
-console.log(parolaArrayReversed)
+console.log(parolaArray)
+const parolaArrayJ = parolaArray.join()
+const parolaArrayReversed = parolaArray.reverse().join()
+console.log(parolaArrayReversed, parolaArrayJ)
 
-if (parolaArrayReversed === parolaArray) {
+if (parolaArrayReversed === parolaArrayJ) {
     alert("Figo! La parola è palindroma")
     } else {
     alert("Peccato!La parola non è palindroma")
